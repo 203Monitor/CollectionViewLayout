b7e254ca4497adc4969ca66794c21ff653b68b66
203monitor <930502900@qq.com>
2017-06-01 18:40:47 +0800

    init

---
+++ CollectionViewPractice/Cell/ShopCell.h
@@ -0,0 +1,17 @@
+//
+//  XMGShopCell.h
+//  04-瀑布流
+//
+//  Created by apple on 14/12/4.
+//  Copyright (c) 2014年 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ModelShop;
+
+@interface ShopCell : UICollectionViewCell
+
+@property (nonatomic, strong) ModelShop *shop;
+
+@end
---
+++ CollectionViewPractice/Model/ModelShop.m
@@ -0,0 +1,13 @@
+//
+//  ModelShop.m
+//  CollectionViewPractice
+//
+//  Created by 武国斌 on 2017/6/1.
+//  Copyright © 2017年 武国斌. All rights reserved.
+//
+
+#import "ModelShop.h"
+
+@implementation ModelShop
+
+@end
